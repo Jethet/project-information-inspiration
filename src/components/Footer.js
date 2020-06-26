a938d69155ca03ce71ea24668d3b693e1862d03f
Jethet <henriette.hettinga@gmail.com>
2020-06-26 15:01:42 +0200

    Add style to search results page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,12 +3,9 @@ import React from "react";
 function Footer() {
   return (
     <div className="footer">
-    <br/><br/><br/>
-    <footer>
-      Credits for pictures and fonts used - HH@2020
-    </footer>
+      <footer>Credits for pictures and fonts used - HH@2020</footer>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
