b8f50d71df220d6093a22fac78ba45f97783e230
Jethet <henriette.hettinga@gmail.com>
2020-07-06 19:00:13 +0200

    Expand search to include any word from card title

--- src/components/SearchCard.js
+++ src/components/SearchCard.js
@@ -4,44 +4,57 @@ import SearchResult from "./SearchResult";
 
 class SearchCard extends React.Component {
   state = {
+    allStorageObjects: [],
     searchResultList: [],
   };
+
+  componentDidMount() {
+    let storageValues = [];
+    let allStorage = [];
+    let keys = Object.keys(localStorage);
+    for (let i = 0; i < keys.length; i++) {
+      storageValues = localStorage.getItem(keys[i]);
+      allStorage.push(JSON.parse(storageValues));
+    }
+    this.setState({ allStorageObjects: allStorage });
+    console.log("The values list from componentDidMount:", allStorage);
+  }
 
   handleSearch = (e) => {
     e.preventDefault();
     // Set the search value to lowercase: card names are saved in local
     // storage with lowercase to avoid search problems with upper/lower
     let searchItem = e.target[0].value.toLowerCase();
-    console.log(searchItem);
     e.target[0].value = "";
 
     let searchResult = "";
-    for (let i = 0; i < localStorage.length; i++) {
-      if (localStorage.getItem(searchItem) !== null) {
-        searchResult = localStorage.getItem(searchItem);
-        console.log(searchResult);
-      } else if (localStorage.getItem(searchItem) === null) {
-        alert("Not found");
-        e.target[0].value = "";
-        return;
-      } else {
-        alert("Something went wrong");
-      }
-    }
+    let storageObjects = this.state.allStorageObjects;
+    let storageStrings = storageObjects.map((item) => {
+      return item.cardTitle.toLowerCase();
+    });
+
+    const foundItem = storageStrings
+      .filter((item) => item.includes(searchItem))
+      .map((item) => {
+        return item;
+      });
+        if (foundItem) {
+          searchResult = localStorage.getItem(foundItem);
+        } else {
+          alert("Not found");
+          return;
+        }
+     
+
+    console.log("searchResult", searchResult);
 
     let parsedSearchResult = JSON.parse(searchResult);
-    
+
     let newSearchResultList = [...this.state.searchResultList];
-    console.log("This is newSearchResultList before push:", newSearchResultList);
-
     newSearchResultList.push(parsedSearchResult);
-    console.log("This is newSearchResultList after push:", newSearchResultList);
-
-    this.setState({ searchResultList: newSearchResultList });
-
-    console.log("This is list of results:", newSearchResultList);
-    console.log("This is the this.state.searchResultList", this.state.searchResultList);
-
+    this.setState({ searchResultList: newSearchResultList }, () =>
+      console.log(this.state.searchResultList)
+    );
   };
 
   render() {
